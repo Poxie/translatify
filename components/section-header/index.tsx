dcc51aee511d4661edd8bb21e533508a273b9307
Poxie <poxen12@hotmail.com>
2024-03-19 00:27:01 +0100

    Add sub header

--- components/section-header/index.tsx
+++ components/section-header/index.tsx
@@ -1,21 +1,42 @@
-import { StyleProp, StyleSheet, TextStyle } from "react-native";
+import { StyleProp, StyleSheet, TextStyle, View } from "react-native";
 import { Text } from "../Themed";
 import FontSizes from "@/constants/FontSizes";
+import useColors from "@/hooks/useColors";
 
-export default function SectionHeader({ children, style }: {
+export default function SectionHeader({ children, subHeader, style }: {
     children: string;
+    subHeader?: string;
     style?: StyleProp<TextStyle>;
 }) {
+    const colors = useColors();
+
     return(
-        <Text style={[styles.text, style]}>
-            {children.toUpperCase()}
-        </Text>
+        <View style={styles.container}>
+            <Text style={[styles.header, style]}>
+                {children.toUpperCase()}
+            </Text>
+            {subHeader && (
+                <Text style={[
+                    styles.subHeader,
+                    { color: colors.muted }
+                ]}>
+                    {subHeader}
+                </Text>
+            )}
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    text: {
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    header: {
         fontSize: FontSizes.small,
         fontWeight: 'bold',
-    }
+    },
+    subHeader: {
+        fontSize: FontSizes.small,
+    },
 })
