b298cabb156743361f290f329c8385102cc607eb
Poxie <poxen12@hotmail.com>
2024-03-19 02:22:07 +0100

    Categorize words

--- app/list.tsx
+++ app/list.tsx
@@ -4,34 +4,70 @@ import Section from "@/components/section";
 import SectionHeader from "@/components/section-header";
 import Spacing from "@/constants/Spacing";
 import { useDatabase } from "@/contexts/database";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { Category, Word } from "@/types";
+import { useMemo } from "react";
+import { StyleSheet, SafeAreaView, View, ScrollView } from "react-native";
 
 export default function ListScreen() {
-    const { words } = useDatabase();
+    const { words, categories } = useDatabase();
+
+    const getWordsByCategoryId = (categoryId: string | null) => (
+        words.filter(word => word.categoryId === categoryId)
+    )
+    const sortCategories = (categories: Category[]) => (
+        categories.sort((a,b) => a.name[0].localeCompare(b.name[0]))
+    ) 
+
+    let allCategories: { id: string | null, name: string }[] = sortCategories(categories);
+    if(words.find(word => !word.categoryId)) {
+        allCategories = [{ id: null, name: 'Uncategorized' }, ...sortCategories(categories)];
+    }
 
     return(
-        <SafeAreaView style={styles.container}>
-            <SectionHeader 
-                subHeader={`${words.length} words`}
-                style={styles.sectionHeader}
-            >
-                Uncategorized
-            </SectionHeader>
-            <Section style={styles.section}>
-                {words.map(word => (
-                    <ListItem 
-                        {...word}
-                        key={word.id}
-                    />
-                ))}
-            </Section>
+        <SafeAreaView>
+            <ScrollView style={styles.container}>
+                {allCategories.map(category => {
+                    const words = getWordsByCategoryId(category.id);
+
+                    return(
+                        <View 
+                            style={styles.sectionContainer} 
+                            key={category.id}
+                        >
+                            <SectionHeader 
+                                subHeader={`${words.length} words`}
+                                style={styles.sectionHeader}
+                            >
+                                {category.name}
+                            </SectionHeader>
+                            <Section style={styles.section}>
+                                {words.map(word => (
+                                    <ListItem 
+                                        {...word}
+                                        key={word.id}
+                                    />
+                                ))}
+                                {words.length === 0 && (
+                                    <Text style={styles.empty}>
+                                        This category is empty.
+                                    </Text>
+                                )}
+                            </Section>
+                        </View>
+                    )
+                })}
+            </ScrollView>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        margin: Spacing.primary,
+        padding: Spacing.primary,
+        paddingTop: 0,
+    },
+    sectionContainer: {
+        marginBottom: Spacing.primary,
     },
     sectionHeader: {
         marginBottom: Spacing.tertiary
@@ -39,5 +75,8 @@ const styles = StyleSheet.create({
     section: {
         padding: Spacing.primary,
         gap: Spacing.secondary
+    },
+    empty: {
+        textAlign: 'center',
     }
 })
