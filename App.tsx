ee1ec2ded94ea7c6fe12a13be840b20e5b047c74
Poxie <poxen12@hotmail.com>
2024-03-29 19:00:06 +0100

    Hide require cycle warnings

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
+import { LogBox } from 'react-native';
 import Navigation from '@/app/index';
 import AuthProvider from './contexts/auth';
 import DatabaseProvider from './contexts/database';
+
+LogBox.ignoreLogs(['Require cycle']);
 
 export default function App() {
   return(
